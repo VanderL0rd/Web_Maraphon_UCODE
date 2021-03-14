812bd0dda2c61678700a824955a5e9dce5d46592
VanderL0rd <78862291+VanderL0rd@users.noreply.github.com>
2021-03-14 16:39:32 +0200

    JavaScript Advanced

--- Sprint 03/t03/js/script.js
+++ Sprint 03/t03/js/script.js
@@ -1,17 +1,79 @@
 class Human {
-    constructor(first, last, gender, age, calories = 250) {
-        this.first_name = first;
-        this.last_name = last;
+    constructor(firstName, lastName, gender, age, calories = 200) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.gender = gender;
         this.age = age;
         this.calories = calories;
+        this.time = function () {
+            let a = setInterval(() => { this.calories -= 200 }, 60000);
+        };
+        this.time();
     }
-    eat = function () {
-        console.log(this.calories);
-        this.calories += 200;
-        setTimeout(() => console.log(this.calories), 10000);
+
+    sleepFor() {
+        document.getElementById("out").innerHTML = 'I am sleeping';
+        setTimeout(() => {
+            document.getElementById("out").innerHTML = "I'm awake now";
+            setTimeout(() => document.getElementById("out").innerHTML = "", 2000);
+        }, 5000);
+    }
+
+    feed() {
+        if (this.calories > 500) {
+            document.getElementById("out_1").innerHTML = 'I am not hungry';
+            setTimeout(() => document.getElementById("out_1").innerHTML = "", 2000);
+        } else if (this.calories < 500) {
+            document.getElementById("out_1").innerHTML = 'Nom nom nom';
+            this.calories = this.calories + 200;
+            if (this.calories < 500) {
+                setTimeout(() => {
+                    document.getElementById("out_1").innerHTML = "I am still hungry";
+                    setTimeout(() => document.getElementById("out_1").innerHTML = "", 2000);
+                }, 10000);
+            } else {
+                setTimeout(() => document.getElementById("out_1").innerHTML = "", 10000)
+            }
+
+        }
+
     }
 }
-var hum = new Human();
-let a = document.getElementById("Btn_1");
-a.onclick = function () { hum.eat(); };
+
+class Superhero extends Human {
+    fly() {
+        document.getElementById("out").innerHTML = 'I am flying';
+        setTimeout(() => document.getElementById("out").innerHTML = "", 10000);
+    }
+
+    fightWithEvil() {
+        document.getElementById("out_1").innerHTML = 'Khhhh-chh..';
+        setTimeout(() => {
+            document.getElementById("out_1").innerHTML = "Bang-g-g-g.";
+            setTimeout(() => {
+                document.getElementById("out_1").innerHTML = "Evil is defeated";
+                setTimeout(() => document.getElementById("out_1").innerHTML = "", 3000);
+            }, 3000);
+        }, 3000);
+    }
+
+}
+var human = new Superhero();
+document.getElementById("btn_3").style.display = "none";
+document.getElementById("btn_4").style.display = "none";
+document.getElementById("btn_1").onclick = function () { human.sleepFor(); };
+document.getElementById("btn_2").onclick = function () { human.feed(); };
+document.getElementById("turn").onclick = function () {
+    document.getElementById("btn_1").style.display = "none";
+    document.getElementById("btn_2").style.display = "none";
+    document.getElementById("btn_3").style.display = "block";
+    document.getElementById("btn_4").style.display = "block";
+    document.getElementById("turn").style.display = "none";
+    document.getElementById("hum").innerHTML = "Superhero";
+}
+document.getElementById("btn_3").onclick = function () {
+    human.fly();
+}
+document.getElementById("btn_4").onclick = function () {
+    human.fightWithEvil();
+}
--- Sprint 03/t05/js/script.js
+++ Sprint 03/t05/js/script.js
@@ -1,13 +1,72 @@
-let questList = new Map();
-questList.set("John White", "Already is list");
-questList.set("Mary", "Already is list");
-questList.set("John Black", "Already is list");
-questList.set("Harry", "Already is list");
+let guestList = new Map();
+guestList.set('John', 'Already in list');
+guestList.set('Suzy', 'Already in list');
+guestList.set('Jane', 'Already in list');
+guestList.set('Rendi', 'Already in list');
 
-console.log("Mary");
-console.log(questList.has("Mary"));
+console.log(guestList);
+console.log(guestList.get('Jane'));
+console.log(guestList.length);
+console.log(guestList.delete('Rendi'));
+console.log(guestList);
 
-console.log("Harry " + questList.get("Harry"));
-console.log("List size: " + questList.size);
-questList.delete("John Black");
-console.log("List size after delete: " + questList.size);
+///////////////////////////////////////////
+
+let menu = new Set();
+
+let borch = { dish: "Borch", price: '120$' };
+let lunch = { dish: "Lunch", price: '50$' };
+let steak = { dish: "Steak", price: '150$' };
+let pizza = { dish: "Pizza", price: '100$' };
+
+menu.add(borch);
+menu.add(lunch);
+menu.add(steak);
+menu.add(pizza);
+menu.add(pizza);
+
+console.log(menu.size);
+
+for (let d of menu) {
+    console.log(d.dish, d.price);
+}
+
+
+//////////////////////////////////////////////
+
+let bankVault = new WeakMap();
+
+let cr2345 = { Name: "Fall" };
+let cr1234 = { Name: "Plot" };
+let cr5678 = { Name: "Akor" };
+let cr9123 = { Name: "Json" };
+
+bankVault.set(cr1234, { Pr: 5682 });
+bankVault.set(cr2345, { Pr: 8435 });
+bankVault.set(cr5678, { Pr: 9725 });
+bankVault.set(cr9123, { Pr: 95768 });
+
+console.log(bankVault.get(cr9123));
+console.log(bankVault.has(cr1234));
+
+
+//////////////////////////////////////////////////
+
+let coinCollection = new WeakSet();
+
+coin_1 = { coin: '¤' };
+coin_2 = { coin: '$' };
+coin_3 = { coin: '£' };
+coin_4 = { coin: '€' };
+
+coinCollection.add(coin_1);
+coinCollection.add(coin_2);
+coinCollection.add(coin_3);
+coinCollection.add(coin_4);
+coinCollection.add(coin_1);
+
+coinCollection.has(coin_2);
+console.log(coinCollection);
+console.log(coinCollection.delete(coin_4));
+console.log(coinCollection);
+
