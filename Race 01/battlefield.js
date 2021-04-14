1745e359417ebb78ea686d3f45d5db1f90e06507
VanderL0rd <vanderlord2@gmail.com>
2021-04-14 12:51:16 +0300

    Card game

--- Race 01/battlefield.js
+++ Race 01/battlefield.js
@@ -1,18 +1,40 @@
 function start() {
     let card_arr = document.getElementsByClassName("cards")[1].getElementsByClassName('card');
     for (let ind = 0; ind < card_arr.length; ind++) {
-        card_arr[ind].onclick = function () {
-            console.log(card_arr[ind]);
-            let par = document.getElementsByClassName('cards')[1]
-            if (card_arr[ind].parentNode == par) {
-                let pl = document.getElementsByClassName('player_pl_cards')[0];
-                pl.appendChild(card_arr[ind]);
-                // par.removeChild(card_arr[ind]);
-            } else if (card_arr[ind].parentElement == document.getElementsByClassName('player_pl_cards')[1]) {
-
+        let card = document.getElementsByClassName("cards")[1].getElementsByClassName('card')[ind];
+        card.onclick = function () {
+            console.log(card);
+            let par = document.getElementsByClassName('cards')[1];
+            if (card.parentNode == par) {
+                let pl = document.getElementsByClassName("player_pl_cards")[0];
+                pl.appendChild(card);
             }
         }
-        console.log(card_arr[ind]);
     }
 }
 start();
+
+let max = 30;
+let num = 0;
+let timer_1 = setInterval(
+    () => {
+        document.getElementsByClassName("Time")[0].innerHTML = "Time: " + (max - num);
+        num++;
+        if (max + 1 == num) {
+            // TO DO: func();
+            clearInterval(timer_1);
+        }
+    }, 1000);
+let timer_0 = setInterval(() => {
+    let max = 30;
+    let num = 0;
+    let timer_1 = setInterval(
+        () => {
+            document.getElementsByClassName("Time")[0].innerHTML = "Time: " + (max - num);
+            num++;
+            if (max + 1 == num) {
+                // TO DO: func();
+                clearInterval(timer_1);
+            }
+        }, 1000);
+}, 31000);
